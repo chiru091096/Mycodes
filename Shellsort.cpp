79ba5e49540f288c6fc055a8d4dfa02caba7da22
chiru091096 <chiru091096@gmail.com>
2017-04-27 11:41:52 +0530

    Add files via upload
    Shellsort in c++ done on 27/04/2017

---
+++ Shellsort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void shellSort(int arr[], int n){
+    int h=1,temp;
+    
+    while(3*h+1<n){
+        h=3*h+1;
+    }
+    
+    while(h>0){
+        for(int i=h;i<n;i++){
+            for(int j=i;j>=h;j-=h){
+                if(arr[j]<arr[j-h]){
+                    temp=arr[j];
+                    arr[j]=arr[j-h];
+                    arr[j-h]=temp;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+        h/=3;
+    }
+}
+
+int main() {
+	int n;
+	cin>>n;
+	int arr[n];
+	
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    shellSort(arr,n);
+    
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+	return 0;
+}
